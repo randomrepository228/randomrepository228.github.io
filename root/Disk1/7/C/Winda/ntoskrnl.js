166ffad9fa452a02842be5786c4bcaa986325942
WindowsVistaUltimate <69724456+WindowsVistaUltimate@users.noreply.github.com>
2022-09-08 10:46:26 +0600

    big pp

--- root/Disk1/7/C/Winda/ntoskrnl.js
+++ root/Disk1/7/C/Winda/ntoskrnl.js
@@ -3,27 +3,34 @@ prevx = 150;
 prevy = 150;
 activewindow;
 class Window {
-    constructor(x, y, width, height, title, innerhtml) {
+    constructor(x, y, width, height, title, innerhtml, icon) {
         this.height = height;
         this.width = width;
         this.title = title;
         this.x = x;
         this.y = y;
         this.innerhtml = innerhtml;
+        this.icon = icon
     }
 }
-function AddWindowDefault(){
-    setTimeout(() => {document.getElementsByClassName("window")[0].className = "window winapi_transparent_nomargin winapi_shadow"}, 300);
-    document.querySelector(".left-bar").innerHTML = '<div class="window-tray"></div>' + document.querySelector(".left-bar").innerHTML
+function AddWindowDefault(icon, num){
+    setTimeout(() => {document.getElementsByClassName("window")[0].className = `n${num} window winapi_transparent_nomargin winapi_shadow`}, 300);
+    document.querySelector(".left-bar").innerHTML = `<div class="n${num} window-tray" onclick="wnd = document.querySelector('.n${num}');if (wnd.style.display == 'none'){wnd.style.display = 'flex'} else{wnd.style.display = 'none'}"><img src=${icon} onerror="this.remove()"></div>` + document.querySelector(".left-bar").innerHTML
 }
+let openedwindows = []
 function AddPopupWindow(window){
+    for(var gg = 0; gg<=openedwindows.length; gg++){}
+    openedwindows.push(gg)
     document.body.innerHTML =
     `
-    <div class="window opening winapi_shadow winapi_transparent_nomargin" style="left: ${window.x}px;top: ${window.y}px; width: ${window.width}; height: ${window.height}">
+    <div class="n${gg} window opening winapi_shadow winapi_transparent_nomargin" style="left: ${window.x}px;top: ${window.y}px; width: ${window.width}; height: ${window.height}">
         <div class="topbar" onmousedown="activewindow = this.parentElement;loop = true;prevx=event.clientX-this.getBoundingClientRect().x;prevy=event.clientY-this.getBoundingClientRect().y" onmouseup="loop = false;">
-            ${window.title}
+            <left>
+                <img src="${window.icon}" onerror="this.style.opacity = '0'">
+                ${window.title}
+            </left>
             <div class="buttons">
-                <div class="dash">&ndash;</div>
+                <div class="dash" onclick="this.parentElement.parentElement.parentElement.style.display = 'none'">&ndash;</div>
                 <div class="square">&#9723;</div>
                 <div class="x" onclick="closeWindow(this.parentElement.parentElement.parentElement)">&#10006;</div>
             </div>
@@ -34,7 +41,7 @@ function AddPopupWindow(window){
         </div>
     </div>
     ` + document.body.innerHTML
-    AddWindowDefault();
+    AddWindowDefault(window.icon, gg);
 }
 // function AddWindow(window){
 
@@ -43,7 +50,8 @@ function closeWindow(window){
     function timeout(){
         window.remove()
     }
-    window.className = "window closing winapi_shadow"
+    window.className += " closing"
+    document.getElementsByClassName(window.className.split(" ")[0])[1].remove()
     setTimeout(timeout, 600); // = 300
 }
 function minimizeWindow(window){
@@ -55,4 +63,4 @@ function move(e){
         activewindow.style.top = `${e.clientY - prevy}px`
     }
 }
-AddPopupWindow(new Window((window.innerWidth/2)-150, (window.innerHeight/2)-150, 300, 300, "Welcome", "Welcome to Windows Beta!"))
+AddPopupWindow(new Window((window.innerWidth/2)-150, (window.innerHeight/2)-150, 300, 300, "Welcome", "Welcome to Windows Beta!", "./img/icon.jpg"))
