428f729fb9fc4e2218baf1e89f325906340df577
randomrepository228 <122028168+randomrepository228@users.noreply.github.com>
2023-09-11 20:07:15 +0600

    mobile support

--- root/Disk1/7/Winda/ntoskrnl.js
+++ root/Disk1/7/Winda/ntoskrnl.js
@@ -18,13 +18,29 @@ function AddWindowDefault(icon, num){
     document.querySelector(".left-bar").innerHTML = `<div class="n${num} window-tray" onclick="wnd = document.querySelector('.n${num}');if (wnd.style.display == 'none'){wnd.style.display = 'flex'} else{wnd.style.display = 'none'}"><img src=${icon} onerror="this.remove()"></div>` + document.querySelector(".left-bar").innerHTML
 }
 let openedwindows = []
+function windowMouseDown(event, elem){
+    let touch = false;
+    activewindow = elem.parentElement
+    loop = true;
+    if (event.touches) {
+        event = event.touches[0];
+        touch = true;
+        console.log(event)
+    }
+    prevx=event.clientX-elem.getBoundingClientRect().x
+    prevy=event.clientY-elem.getBoundingClientRect().y
+    if (!touch)
+        document.addEventListener("mouseup", () => {loop = false}, {once: true});
+    else
+        document.addEventListener("touchend", () => {loop = false}, {once: true});
+}
 function AddPopupWindow(window){
     for(var gg = 0; gg<=openedwindows.length; gg++){}
     openedwindows.push(gg)
     document.body.innerHTML =
     `
     <div class="n${gg} window opening winapi_shadow winapi_transparent_nomargin" style="left: ${window.x}px;top: ${window.y}px; width: ${window.width}px; height: ${window.height}px">
-        <div class="topbar" onmousedown="activewindow = this.parentElement;loop = true;prevx=event.clientX-this.getBoundingClientRect().x;prevy=event.clientY-this.getBoundingClientRect().y" onmouseup="loop = false;">
+        <div class="topbar" onmousedown="windowMouseDown(event, this)" ontouchstart="windowMouseDown(event, this)">
             <left>
                 <img src="${window.icon}" onerror="this.remove()">
                 ${window.title}
@@ -58,6 +74,7 @@ function minimizeWindow(window){
     window.style.display = "none";
 }
 function move(e){
+    if (e.touches) e = e.touches[0]
     if(loop){
         activewindow.style.left = `${e.clientX - prevx}px`
         activewindow.style.top = `${e.clientY - prevy}px`
