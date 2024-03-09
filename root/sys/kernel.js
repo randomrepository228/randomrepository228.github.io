56e83f8350667cc95346022c008cb499fc7c2205
randomrepository228 <niceman4576@gmail.com>
2024-03-09 19:42:49 +0500

    message fix

--- root/sys/kernel.js
+++ root/sys/kernel.js
@@ -169,7 +169,7 @@ function desktopInit(){
     loadApp("sfc", undefined, "/silent")
     init = true
     if(!localStorage.prevver || localStorage.prevver != localStorage.ver){
-        msgbox("New update", "<h1 style=\"margin: 0\">Welcome to 20240301!</h1>What's new?<br><ul><li>Now you can drag taskbar to change it's position!</li></ul>")
+        msgbox("New update", "<h1 style=\"margin: 0\">Welcome to 20240309!</h1>What's new?<br><ul><li>New theme: Luna</li><li></li></ul>")
         localStorage.prevver = localStorage.ver
         return;
     }
